26d8cdabb96a6f10807fd37825035936916513a8
Nithinhpe <nithin.chandru@hpe.com>
2023-04-19 15:14:32 +0530

    commit 3

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,8 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven/java-app
 echo "$WORKSPACE"
-docker run --rm  -v  $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+/usr/bin/docker login -u nithinhpe -p Nithin@1271
+docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker logout
+              
