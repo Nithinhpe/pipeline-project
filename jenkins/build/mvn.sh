42999ef213b6876b0ec28dac56cafbde087c92ab
Nithinhpe <nithin.chandru@hpe.com>
2023-04-19 15:22:02 +0530

    commit4

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,7 +5,7 @@ echo "** Building jar ***********"
 echo "***************************"
 
 echo "$WORKSPACE"
-/usr/bin/docker login -u nithinhpe -p Nithin@1271
+/usr/bin/docker login docker.io -u nithinhpe -p Nithin@1271
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 docker logout
               
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,6 @@ echo "***************************"
 echo "** Testing the code ***********"
 echo "***************************"
 
-
+/usr/bin/docker login docker.io -u nithinhpe -p Nithin@1271
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker logout
